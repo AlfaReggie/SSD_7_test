91619a112a674bb0d2a07e25531b1a1187e49460
AlfaReggie <107858720+AlfaReggie@users.noreply.github.com>
2022-11-21 21:07:16 +0300

    Update ssd_model_new.py

--- traind_ssd/ssd_model_new.py
+++ traind_ssd/ssd_model_new.py
@@ -39,15 +39,15 @@ def fpn(out1, out2, out3):
     out3 = keras.layers.Conv2D(128, 1, 1, "same")(out3)
     out2 = out2 + keras.layers.UpSampling2D(2)(out3)
     #out2 = keras.layers.BatchNormalization()(out2)
-    out2 = keras.layers.Conv2D(128, 2, 2, "same")(out2)
-    out2 = keras.layers.Conv2DTranspose(128, 2, 2, 'same')(out2)
+    out2 = keras.layers.Conv2D(128, 1, 2, "same")(out2)
+    out2 = keras.layers.Conv2DTranspose(128, 1, 2, 'same')(out2)
     out1 = out1 + keras.layers.UpSampling2D(2)(out2)
     #out1 = keras.layers.BatchNormalization()(out1)
-    out1 = keras.layers.Conv2D(128, 2, 2, "same")(out1)
-    out1 = keras.layers.Conv2DTranspose(128, 2, 2, 'same')(out1)
-    out1 = keras.layers.Conv2D(256, 3, 3, "same")(out1)
-    out2 = keras.layers.Conv2D(256, 3, 3, "same")(out2)
-    out3 = keras.layers.Conv2D(256, 3, 3, "same")(out3)
+    out1 = keras.layers.Conv2D(128, 1, 2, "same")(out1)
+    out1 = keras.layers.Conv2DTranspose(128, 1, 2, 'same')(out1)
+    out1 = keras.layers.Conv2D(256, 3, 2, "same")(out1)
+    out2 = keras.layers.Conv2D(256, 3, 2, "same")(out2)
+    out3 = keras.layers.Conv2D(256, 3, 2, "same")(out3)
     return out1, out2, out3
    
     
