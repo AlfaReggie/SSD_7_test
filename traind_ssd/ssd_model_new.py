65ac9d833b89285c1c86a7b8b35f163f58dabc6a
AlfaReggie <107858720+AlfaReggie@users.noreply.github.com>
2022-12-05 17:51:15 +0300

    Update ssd_model_new.py

--- traind_ssd/ssd_model_new.py
+++ traind_ssd/ssd_model_new.py
@@ -37,6 +37,10 @@ def fpn(out1, out2, out3):
     out1 = keras.layers.Conv2D(128, 1, 1, "same")(out1)
     out2 = keras.layers.Conv2D(128, 1, 1, "same")(out2)
     out3 = keras.layers.Conv2D(128, 1, 1, "same")(out3)
+    
+    out3 = out3 + keras.layers.UpSampling2D(2)(out1)
+    out3 = keras.layers.BatchNormalization()(out3)
+    out3 = keras.layers.Conv2D(128, 2, 1, "same")(out3)
     out2 = out2 + keras.layers.UpSampling2D(2)(out3)
     out2 = keras.layers.BatchNormalization()(out2)
     out2 = keras.layers.Conv2D(128, 2, 1, "same")(out2)
@@ -44,8 +48,8 @@ def fpn(out1, out2, out3):
     out1 = keras.layers.BatchNormalization()(out1)
     out1 = keras.layers.Conv2D(128, 2, 1, "same")(out1)
       
-    out1 = keras.layers.Conv2D(256, 3, 2, "same")(out1)
-    out2 = keras.layers.Conv2D(256, 3, 2, "same")(out2)
+    out1 = keras.layers.Conv2D(512, 3, 2, "same")(out1)
+    out2 = keras.layers.Conv2D(512, 3, 2, "same")(out2)
     out3 = keras.layers.Conv2D(512, 3, 2, "same")(out3)
     return out1, out2, out3
       
